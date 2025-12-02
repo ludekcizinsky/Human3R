3de7b3d2c6dc488f0f9c9845013ffc501bd4776c
LudekCizinsky <ludekcizinsky11@gmail.com>
2025-12-02 17:56:49 +0100

    initial inference pipeline

--- run_inference.sh
+++ run_inference.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
+set -euo pipefail
 
 module load gcc ffmpeg
 
 source /home/cizinsky/miniconda3/etc/profile.d/conda.sh
 conda activate human3r
+module load gcc ffmpeg
 
 # Configurations
-SCENE_NAME="football_high_res"
-SEQ_PATH="/scratch/izar/cizinsky/multiply-output/preprocessing/data/$SCENE_NAME/image"
-MODEL_PATH="/scratch/izar/cizinsky/pretrained/human3r.pth"
+SCENE_NAME="hi4d_pair00_fight"
+BASE_DIR="/scratch/izar/cizinsky/thesis/preprocessing/$SCENE_NAME/lhm"
+SEQ_PATH="$BASE_DIR/frames"
+MODEL_PATH="/scratch/izar/cizinsky/pretrained/human3r_896L.pth"
+OUTPUT_DIR="$BASE_DIR/motion_human3r"
+mkdir -p "$OUTPUT_DIR"
+
 SIZE=512
 SUBSAMPLE=1
 VIS_THRESHOLD=2
 DOWNSAMPLE_FACTOR=1
 RESET_INTERVAL=100
-OUTPUT_DIR="/scratch/izar/cizinsky/multiply-output/preprocessing/data/$SCENE_NAME/human3r"
-mkdir -p $OUTPUT_DIR
 
 echo "Running inference"
 echo "---------------------------------------------------------------------------------------------"
-# Run inference
-CUDA_VISIBLE_DEVICES=0 python demo.py \
-    --model_path $MODEL_PATH \
-    --size $SIZE \
-    --seq_path $SEQ_PATH \
-    --subsample $SUBSAMPLE \
+python inference.py \
+    --model_path "$MODEL_PATH" \
+    --size "$SIZE" \
+    --seq_path "$SEQ_PATH" \
+    --subsample "$SUBSAMPLE" \
     --use_ttt3r \
-    --vis_threshold $VIS_THRESHOLD \
-    --downsample_factor $DOWNSAMPLE_FACTOR \
-    --reset_interval $RESET_INTERVAL \
-    --output_dir $OUTPUT_DIR \
+    --vis_threshold "$VIS_THRESHOLD" \
+    --downsample_factor "$DOWNSAMPLE_FACTOR" \
+    --reset_interval "$RESET_INTERVAL" \
+    --output_dir "$OUTPUT_DIR" \
     --save_video \
     --save_smpl
